62dedf601f5ebb0ff6a7c45a9058cbe2539ed77e
zan.rosenthal <zan.rosenthal@gmail.com>
2017-09-10 10:32:44 -0500

    Color blocks v1

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,34 +5,27 @@ import styles from '../styles/main.module.scss'
 
 export default ({ children }) =>
   <div className={styles.outerContainer}>
-    {/* <script src="https://d3gxy7nm8y4yjr.cloudfront.net/js/embed.js" type="text/javascript"></script> */}
-    <div className={styles.innerContainer} >
-    <div className={styles.headerWrapper}>
-      <header className={styles.header}>
-        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <div className={styles.siteTitle}>
-            <h1>Trent Crestwell</h1>
-            <h5>Guild Certified Feldenkrais Praticioner</h5>
-          </div>
-        </Link>
-        <ul className={styles.nav}>
-          <ListLink
-            listClass={styles.linkContainer}
-            linkClass={styles.navLink}
-            to="/classes/">Classes</ListLink>
-          <ListLink
-            listClass={styles.linkContainer}
-            linkClass={styles.navLink}
-            to="/appointments/">Appointments</ListLink>
-          <ListLink
-            listClass={styles.linkContainer}
-            linkClass={styles.navLink}
-            to="/about-trent/">About Trent</ListLink>
-        </ul>
-      </header>
-      </div>
-    <div className={styles.contentWrapper}>
-      {children()}
-    </div>
-  </div>
+    <header className={styles.header}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <div className={styles.siteTitle}>
+          <h1>Trent Crestwell</h1>
+          <h5>Guild Certified Feldenkrais Praticioner</h5>
+        </div>
+      </Link>
+      <ul className={styles.nav}>
+        <ListLink
+          listClass={styles.linkContainer}
+          linkClass={styles.navLink}
+          to="/classes/">Classes</ListLink>
+        <ListLink
+          listClass={styles.linkContainer}
+          linkClass={styles.navLink}
+          to="/appointments/">Appointments</ListLink>
+        <ListLink
+          listClass={styles.linkContainer}
+          linkClass={styles.navLink}
+          to="/about-trent/">About Trent</ListLink>
+      </ul>
+    </header>
+    {children()}
 </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,21 @@ import cx from 'classnames'
 
 
 export default () =>
-  <div className={styles.contentBody}>
-    <div>
-    <h1
-      className={cx(styles.pageTitle, styles.header)}>
-      What is the Feldenkrais Method</h1>
-    <p className={styles.content}>
-      The Feldenkrais Method is a movement method which emphasizes functionality and ease of movement. The method builds an awareness of parasitic habitual patterns in order to find new strategies towards more comfortable movement .
-    </p>
-    <p className={styles.content}>
-      The method is useful for anyone with a body. Whether you are recovering from injury, seeking relief from a physical or emotional condition, trying to be more productive at work, or honing athletic or artistic abilities, Feldenkrais (can help/has something to offer/etc?)
-    </p>
-    <h5>The Feldenkrais Method consists of two practices: </h5>
+    <div className={styles.contentBody}>
+    <div className={styles.pageContent}>
+      <h1
+        className={cx(styles.pageTitle, styles.header)}>
+        What is the Feldenkrais Method</h1>
+      <p className={styles.content}>
+        The Feldenkrais Method is a movement method which emphasizes functionality and ease of movement. The method builds an awareness of parasitic habitual patterns in order to find new strategies towards more comfortable movement .
+      </p>
+      <p className={styles.content}>
+        The method is useful for anyone with a body. Whether you are recovering from injury, seeking relief from a physical or emotional condition, trying to be more productive at work, or honing athletic or artistic abilities, Feldenkrais (can help/has something to offer/etc?)
+      </p>
+      <h5>The Feldenkrais Method consists of two practices: </h5>
+    </div>
     <div className={styles.boxContainer}>
-      <div className={styles.box}>
+      <div className={cx(styles.box, styles.leftBox)}>
         <ListLink
           listClass={styles.linkContainer}
           linkClass={styles.bodyLink}
@@ -30,7 +31,7 @@ export default () =>
           See Class Schedule
         </ListLink>
       </div>
-      <div className={styles.box}>
+      <div className={cx(styles.box, styles.rightBox)}>
         <ListLink
           listClass={styles.linkContainer}
           linkClass={styles.bodyLink}
@@ -43,5 +44,4 @@ export default () =>
         </ListLink>
       </div>
     </div>
-  </div>
   </div>
