182acdbcdd95c1031a525764daf79b7d4ef6e711
zan.rosenthal <zan.rosenthal@gmail.com>
2017-09-11 08:25:11 -0500

    Color blocks v2

--- src/pages/appointments.js
+++ src/pages/appointments.js
@@ -4,21 +4,23 @@ import ListLink from '../components/ListLink'
 import cx from 'classnames'
 
 export default () =>
+<div>
     <div
-      className={styles.contentBody}>
+      className={styles.pageContent}>
       <div className="content">
         <h1 className={styles.pageTitle}>Appointments</h1>
         <h3 className={styles.secondaryTitle}>Functional Integration is...</h3>
         <p className={styles.content}>the one-on-one application of the method that uses both touch and verbal instruction from the practitioner. The sessions begin with a brief interview in which the client can identify their interests and goals so the practitioner can create a lesson specific to the clients needs. </p>
       </div>
-      <div className={styles.schedulingWidget}>
-        <a
-          className={cx(styles.bodyLink,styles.linkContainer)}
-          target="_blank"
-          href="https://trent-crestwell.acuityscheduling.com/">
-        <h4 className={styles.linkTitle}>
-          Schedule an Appointment
-        </h4>
-      </a>
-      </div>
     </div>
+    <div className={cx(styles.schedulingWidget, styles.box, styles.rightBox)}>
+      <a
+        className={cx(styles.bodyLink,styles.linkContainer)}
+        target="_blank"
+        href="https://trent-crestwell.acuityscheduling.com/">
+      <h4 className={styles.linkTitle}>
+        Schedule an Appointment
+      </h4>
+    </a>
+    </div>
+</div>
