d3a498ad316e37a29861cf81fda44957e869aa3c
zan.rosenthal <zan.rosenthal@gmail.com>
2017-10-09 11:41:46 -0500

    With new font and colors

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,24 +1,37 @@
 import React from 'react'
 import styles from '../../styles/main.module.scss'
 import ListLink from '../ListLink'
+import cx from 'classnames'
+import windowSize from 'react-window-size'
 
-export default function Nav () {
-  return (
-    <div className={styles.nav} >
-      <ul className={styles.navList}>
-      <ListLink
-        listClass={styles.linkContainer}
-        linkClass={styles.navLink}
-        to="/classes">Classes</ListLink>
-      <ListLink
-        listClass={styles.linkContainer}
-        linkClass={styles.navLink}
-        to="/appointments">Appointments</ListLink>
-      <ListLink
-        listClass={styles.linkContainer}
-        linkClass={styles.navLink}
-        to="/about-trent">About Trent</ListLink>
-      </ul>
-    </div>
-  )
+const renderNavList = () => (
+  <ul className={styles.navList}>
+    <ListLink
+      listClass={styles.linkContainer}
+      linkClass={styles.navLink}
+      to="/classes/">Classes</ListLink>
+    <ListLink
+      listClass={styles.linkContainer}
+      linkClass={styles.navLink}
+      to="/appointments/">Appointments</ListLink>
+    <ListLink
+      listClass={styles.linkContainer}
+      linkClass={styles.navLink}
+      to="/about-trent/">About Trent</ListLink>
+  </ul>
+)
+
+
+class Nav extends React.PureComponent {
+
+  render () {
+    console.log(this.state)
+    return (
+      <div className={styles.nav} >
+        {renderNavList()}
+      </div>
+    )
+  }
 }
+
+export default windowSize(Nav)
